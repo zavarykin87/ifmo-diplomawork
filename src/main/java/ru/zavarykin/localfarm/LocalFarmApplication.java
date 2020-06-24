915fddd6623a8111ae5b4737900f723a80281039
Andrew Zavarykin <zavarykin87@gmail.com>
2020-06-24 10:40:40 +0300

    Initial commit

---
+++ src/main/java/ru/zavarykin/localfarm/LocalFarmApplication.java
@@ -0,0 +1,13 @@
+package ru.zavarykin.localfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zavarykin/localfarm/LocalFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zavarykin.localfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
