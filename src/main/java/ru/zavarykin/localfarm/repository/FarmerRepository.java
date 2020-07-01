2f6d9c3a7e6f87200f5afe4ff537aac4cf48baef
Andrew Zavarykin <zavarykin87@gmail.com>
2020-07-01 20:21:51 +0300

    добавил entity фермера

---
+++ src/main/java/ru/zavarykin/localfarm/repository/FarmerRepository.java
@@ -0,0 +1,8 @@
+package ru.zavarykin.localfarm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.zavarykin.localfarm.entity.Farmer;
+
+
+public interface FarmerRepository extends JpaRepository<Farmer, Integer> {
+}
